01300f3b3523cc25c94abb34869149e2a8a646f7
matthew hallberg <matthewhallberg@gmail.com>
2018-08-18 21:44:28 -0400

    added buttons and another model.

---
+++ Assets/Scripts/Explode.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explode : MonoBehaviour {
+
+	private bool exploded = false;
+
+	private void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Explosion ();
+		}
+	}
+
+	public void OnContentPlaced () {
+		StartCoroutine (PlacedRoutine ());
+	}
+
+	IEnumerator PlacedRoutine () {
+		yield return new WaitForSeconds (1f);
+		Explosion ();
+	}
+
+	//Force rigid body explosion
+	public void Explosion () {
+		if (!exploded) {
+			exploded = true;
+			//add physics and stuff to children
+			foreach (Transform child in this.transform) {
+				child.gameObject.AddComponent<Rigidbody> ();
+				child.gameObject.AddComponent<BoxCollider> ();
+				child.gameObject.AddComponent<ParticleBehavior> ();
+			}
+		}
+	}
+
+	public void SetExplosionFrame (float frame) {
+		ParticleBehavior[] particles = FindObjectsOfType<ParticleBehavior> ();
+		foreach (ParticleBehavior particle in particles) {
+			particle.SetFrameCount (Mathf.RoundToInt(frame));
+		}
+	}
+}
